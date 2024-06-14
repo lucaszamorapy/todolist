184c5b76d5265d3ee079772cd855b679b044b0a4
Lucas Zamora <113631100+lucaszamorapy@users.noreply.github.com>
2024-06-14 19:20:08 -0300

    feat: create a functions in tarefa component

--- src/components/tarefas/TarefaLista.vue
+++ src/components/tarefas/TarefaLista.vue
@@ -1,25 +1,35 @@
 <template>
   <div>
-    <v-list-item>
-          <template v-slot:default="{ active }">
-            <v-list-item-action>
-              <v-checkbox :input-value="active"></v-checkbox>
-            </v-list-item-action>
+    <v-list-item
+      :class="{ 'blue lighten-4': tarefa.concluido }"
+      @click="toggleConcluido"
+    >
+      <template v-slot:default="{}">
+        <v-list-item-action>
+          <v-checkbox :input-value="tarefa.concluido"></v-checkbox>
+        </v-list-item-action>
 
-            <v-list-item-content>
-              <v-list-item-title>Notifications</v-list-item-title>
-            </v-list-item-content>
-          </template>
-        </v-list-item>
+        <v-list-item-content>
+          <v-list-item-title
+            :class="{ '.text-decoration-line-through': tarefa.concluido }"
+            >{{ tarefa.titulo }}</v-list-item-title
+          >
+        </v-list-item-content>
+      </template>
+    </v-list-item>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: ["tarefa"],
+  methods: {
+    toggleConcluido() {
+      this.$emit("update-concluido");
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
--- src/views/TarefasView.vue
+++ src/views/TarefasView.vue
@@ -1,28 +1,62 @@
 <template>
   <div>
-    <v-list
-      flat
-      subheader
-      
-    >
-      <v-list-item-group
-        v-model="settings"
-        multiple
-        active-class=""
-      >
-        <TarefaLista />
+    <v-col cols="12">
+      <v-text-field
+        v-model="campoInput"
+        label="Qual sua tarefa de hoje?"
+        @keyup.enter="handleAddTarefa"
+      ></v-text-field>
+    </v-col>
+
+    <v-list flat subheader>
+      <v-list-item-group multiple active-class="">
+        <div v-for="(tarefa, index) in tarefas" :key="index">
+          <TarefaLista
+            :tarefa="tarefa"
+            @update-concluido="updateTarefaConcluido(index)"
+          />
+        </div>
       </v-list-item-group>
     </v-list>
   </div>
 </template>
 
 <script>
-  import TarefaLista from "../components/tarefas/TarefaLista.vue"
-  export default {
-    name: 'HomeView',
+import TarefaLista from "../components/tarefas/TarefaLista.vue";
 
-    components: {
-      TarefaLista
+export default {
+  name: "HomeView",
+  components: {
+    TarefaLista,
+  },
+  data() {
+    return {
+      campoInput: null,
+      tarefas: [
+        {
+          titulo: "Ir ao mercado",
+          concluido: false,
+        },
+        {
+          titulo: "Comprar ração",
+          concluido: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    handleAddTarefa() {
+      if (this.campoInput) {
+        this.tarefas.push({ titulo: this.campoInput, concluido: false });
+        this.campoInput = null; // Limpar o campo de input após adicionar
+      }
     },
-  }
+    updateTarefaConcluido(index) {
+      this.tarefas[index].concluido = !this.tarefas[index].concluido;
+    },
+  },
+};
 </script>
+
+<style>
+</style>
